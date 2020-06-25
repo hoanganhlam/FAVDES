4346cfd44acff648ecd326cd2413ecb9da7f1ad6
lamha <lam@trip.vn>
2020-06-25 16:44:02 +0700

    OK

--- favdes/settings.py
+++ favdes/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DBNAME', 'favdes'),
         'USER': os.getenv('POSTGRES_USER', 'lam'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'HoangAnhLam@no99a'),
-        'HOST': os.getenv('POSTGRES_HOST', '209.97.166.85'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
